916074873e6c52168fec190098a213a7171c466a
Hourn Sovannara <hournsovannara18@kit.edu.kh>
2020-09-27 19:25:26 +0700

    Fix Heroku Attemp 2nd

--- server/index.js
+++ server/index.js
@@ -18,7 +18,9 @@ if(process.env.NODE_ENV === 'production' ){
 
     app.use(express.static(__dirname +'/public/'));
 
-    app.get(/.*/,(req,res=> res.sendFile(__dirname + '/public/index.html')));
+    app.get(/.*/,(
+        (req,res)=> res.sendFile(__dirname + '/public/index.html')
+        ));
 }
 
 const port = process.env.PORT || 5000;
